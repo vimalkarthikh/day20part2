1f3e7667b6ce6e63c9d79a822c13009bda2abdf1
vimalkarthikh <137371585+vimalkarthikh@users.noreply.github.com>
2023-07-29 13:12:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 async function foo(){
-    var res= await fetch("http://api.citybik.es/v2/networks");
+    var res= await fetch("https://isro.vercel.app/api/centres");
     var result=await res.json();
     console.log(result);
-   
-    let a=result.networks;
+    
+    let a=result.centres;
 
-    console.log(result.networks[1]);
-
-    a.forEach(element => {        
-            var div=document.createElement("div");
-            div.innerHTML=`
-            <div class="card" style="max-width: 22rem;">
-  <div class="card-header"><h4>${element.name}</h4></div>
-  <div class="card-body">
-  <h6>Company: ${element.company}</h6>
-  <h6>ID: ${element.id}</h6>
-  <h6>City: ${element.location.city}</h6>
-  <h6>Country code: ${element.location.country}</h6>
-  <h6>Latitude: ${element.location.latitude}</h6>
-  <h6>Longitude: ${element.location.longitude}</h6>
-  <h6>Website: <a href="${element.href}"> Visit Our Website</a></h6>
+      a.forEach(element => {   
+      console.log(element.id)     
+      console.log(element.country)    
+      var div=document.createElement("div");
+      div.innerHTML=`
+      
+  
+  <div class="card" style="width: 18rem;">
+  <div class="card-header">
+    <b><i>ISRO Centres</i></b>
   </div>
-</div>`
-            document.body.append(div);
+  <img src="img/3517454.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+  <b>ISRO id: </b><i>  ${element.id}</i><br> 
+  <b>State: </b><i>  ${element.State}</i><br>
+  <b>City: </b><i>  ${element.Place}</i><br> 
+  <b>Centre for: </b><i><br>  ${element.name}</i><br>
+  <a href="https://www.isro.gov.in/"> Visit us </a>   
+  </div>
+</div>
+
+`;
+      document.body.append(div); 
         
 
     });
